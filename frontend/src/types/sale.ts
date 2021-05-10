f3637f01211c32fbf9ba27c6ba2e18d9fc385188
INFOWAP <adaomasso@gmail.com>
2021-05-10 02:51:52 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,11 @@
-import { type } from "os";
 
 export type SaleSum = {
     sellerName: string,
     sum: number
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://localgost:8080';
+export const BASE_URL = 'http://localhost:8080';
